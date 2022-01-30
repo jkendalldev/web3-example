4b8315658c93b15031ce62843b6546989e389b7c
Jason <jkendall1975@gmail.com>
2022-01-30 09:35:11 -0500

    web3-example app get latest block number

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+async function main() {
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3("https://eth-mainnet.alchemyapi.io/v2/<>");
+    const blockNumber = await web3.eth.getBlockNumber();
+    console.log("The latest block number is " + blockNumber);
+   }
+   main();             
